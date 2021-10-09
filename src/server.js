42dcd42ed9d5830ac9920e85bf6e79437c1ae84f
tonyfrenzy <tonyfrenzy@gmail.com>
2021-10-09 01:19:07 +0100

    add package.json & editor config

---
+++ src/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import router from './routes/router.js';
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// API Index
+app.get('/', (req, res) => {
+  res.json({
+    status: 'success',
+    message: 'Welcome to Our Food Management API.',
+    description: 'Stutern 1.0 Inter-track Group 5 Project (Backend).',
+
+  });
+});
+
+app.use(router);
+// app.use('/api/v1', router);
+
+export default app;
