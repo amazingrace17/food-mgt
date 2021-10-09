0061a8147d874b1f4c9f4a59dfd75e496c0986d8
tony <tonyfrenzy@gmail.com>
2021-10-09 13:47:45 +0100

    Revert "install packages and configure CI"

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import app from './server.js';
-import dbConnection from './db.js';
-const port = process.env.PORT || 7000;
-
-dbConnection.getConnect();
-
-app.listen(port, () => {
-  console.log(`Server connected at  http://localhost:${port}`);
-});
