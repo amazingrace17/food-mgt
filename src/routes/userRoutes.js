ed86a5bdd9a5a5833b159a46cbc0c4f9e5335160
tonyfrenzy <tonyfrenzy@gmail.com>
2021-10-09 14:49:43 +0100

    update

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,8 @@
 import { Router } from 'express';
-import cors from 'cors';
-import AuthController from '../controllers/AuthController.js';
-// import authValidator from '../middlewares/AuthValidator.js';
+import AuthController from '../controllers/AuthController';
 
 const router = Router();
 
-router.route('/register')
-    .post(AuthController.signupUser);
+router.route('/register').post(AuthController.signupUser);
 
 export default router;
