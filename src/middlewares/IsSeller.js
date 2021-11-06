5923c94e7b64beb5dfab1914b3117e71f359f228
tonyfrenzy <tonyfrenzy@gmail.com>
2021-11-06 15:07:25 +0100

    auth middlewares update

--- src/middlewares/IsSeller.js
+++ src/middlewares/IsSeller.js
@@ -2,20 +2,18 @@
 const IsSeller = async(req, res, next) => {
   // Always combine with AuthValidator to access req.user
   try {
-    console.log(req.user);
-    
-    if (!req.user.role === 'seller') {
-        return res.status(401).json({ 
-            status: "failed", 
-            message: "unauthorized: sellers access only"
-        });
+    if (!req.user.role || req.user.role !== 'seller') {
+      return res.status(401).json({ 
+        status: "failed", 
+        message: "unauthorized: sellers access only"
+      });
     }
 
     next();
   } catch (error) {
     return res.status(500).json({ 
-        status: "failed", 
-        message: error.message 
+      status: "failed", 
+      message: error.message 
     });
   }
 };
